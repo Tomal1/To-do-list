b1e81e8a8d445935cba6de8b874ee384100e5c66
Minhazul Islam <minhazulislam@hotmail.com>
2023-01-06 23:06:59 +0000

    input can now render text and create delete/edit buttons too

--- app.js
+++ app.js
@@ -1,2 +1,29 @@
-outputCon = document.querySelector("#outputContainer");
+let outputCon = document.querySelector("#outputContainer");
+let inputText = document.querySelector("#inputText");
+let addButton = document.querySelector("#addButton");
+
+addButton.addEventListener("click", () =>{
+
+    let output = document.createElement("p");
+    output.classList.add("outputText");
+    output.innerHTML = inputText.value;
+    outputCon.appendChild(output);
+
+    let deleteBtn  =  document.createElement("button");
+    deleteBtn.classList.add("deleteBtn");
+    deleteBtn.innerHTML = "Delete";
+    outputCon.appendChild(deleteBtn);
+
+    let editBtn  =  document.createElement("button");
+    editBtn.classList.add("editBtn");
+    editBtn.innerHTML = "Edit";
+    outputCon.appendChild(editBtn);
+
+});
+
+
+
+
+
+
 
