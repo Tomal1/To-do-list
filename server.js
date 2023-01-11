0141c24d8885a2288da40ce0e2a98e3553a11044
Minhazul Islam <minhazulislam@hotmail.com>
2023-01-11 21:38:34 +0000

    temporary database made, public foleder conncted and get request added

--- server.js
+++ server.js
@@ -1,15 +1,32 @@
-const express =  require("express");
+const fs = require("fs");
 
-const PORT = 3001;
+const express = require("express");
+const path = require("path");
 
+const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.listen("PORT", ()=>{
+//connects public folder
+app.use(express.static("./public"));
+
+const data1 = JSON.parse(fs.readFileSync(`${__dirname}/data/data.json`));
+
+app.get("/api/data", (req,res)=>{
+    res.status(200).json({
+        status:"success", //optional gives message in isomnia to say it has been successfully sent
+        result: data1.length, //optional gives data on how many objects in data1
+        data: {
+            data: data1
+        }
+    })
+});
+
+
+
+
+app.listen(PORT, ()=>{
     console.log(`listening to PORT ${PORT}`)
-})
-
-
-
-
-
+});
