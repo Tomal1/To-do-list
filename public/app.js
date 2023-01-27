6f236320c58fe4085e604fd32cef455a805f2b03
Minhazul Islam <minhazulislam@hotmail.com>
2023-01-27 01:11:38 +0000

    get, post fetch render to both database and interface, edit button renders an input to the  interface

--- public/app.js
+++ public/app.js
@@ -2,27 +2,90 @@ let outputCon = document.querySelector("#outputContainer");
 let inputText = document.querySelector("#inputText");
 let addButton = document.querySelector("#addButton");
 
-addButton.addEventListener("click", () =>{
-
-    let output = document.createElement("p");
-    output.classList.add("outputText");
-    output.innerHTML = inputText.value;
-    outputCon.appendChild(output);
+const createTodoItem = (value) => {
+    let output = document.createElement("p"); //creating a paragraph tag
+    output.classList.add("outputText"); //giving paragraph a class
+    
+    //use text content instead because inner htm is for elements
+    output.textContent = value.message; //what we want inside the paragraph
+    outputCon.appendChild(output); //where we want to add it
 
     let deleteBtn  =  document.createElement("button");
     deleteBtn.classList.add("deleteBtn");
     deleteBtn.innerHTML = "Delete";
+    deleteBtn.dataset.itemId = value.id //
     outputCon.appendChild(deleteBtn);
 
     let editBtn  =  document.createElement("button");
+    editBtn.dataset.itemId = value.id; //giving edit btn an id
+   
+
+    editBtn.addEventListener("click", (e) => {
+        let input = document.createElement("input");
+        input.type = "text";
+        input.className = "input";
+        outputCon.appendChild(input);
+
+        let done = document.createElement("button");
+        done.innerHTML = "done";
+        outputCon.appendChild(done);
+        
+        fetch("/api/data/" + e.target.dataset.itemId, {
+            method: "PATCH",
+            headers: {
+                "Content-Type":"application/json"
+            },
+        })
+        // .then(() => lo)
+
+    })
+    
     editBtn.classList.add("editBtn");
     editBtn.innerHTML = "Edit";
     outputCon.appendChild(editBtn);
+}
+
+addButton.addEventListener("click", () =>{
+    ///converts inputText.value value into an object
+    const convertToObj = {
+        message: inputText.value
+    }
+
+    console.log(convertToObj)
+    
+    fetch("/api/data",{
+        method:"POST", 
+        ///use this or body wont append to database (this is letting api know what type of content is being parsed)
+        headers: {
+            "Content-Type":"application/json"
+        },
+        // a JSON object can only take string so this method turns everything into a string
+        body: JSON.stringify(convertToObj)
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+      const items = data.data.data;
+      createTodoItem()
+    })    
 });
 
 
-
-
-
-
+const name = () =>{
+    // GET method
+    fetch("/api/data", {
+        method: "GET" //is calling the get api in server.js, thats how it know which file to go inside
+    })
+    .then(response =>response.json()) //convert that data into json body
+    .then(data =>{
+        console.log(data)
+        const items = data.data.data;
+        // loop through all the data in 
+        for (let i = 0; i < items.length; i++) {
+            const dataElement = items[i];
+            createTodoItem(dataElement)
+        }
+    })
+};
+name()
 
--- server.js
+++ server.js
@@ -6,54 +6,67 @@ const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json());// middleware to allow us to use json 
+app.use(express.urlencoded({ extended: true })); //middle where to allow us to use special characters in url
 
 //connects public folder
 app.use(express.static("./public"));
 
-const data1 = JSON.parse(fs.readFileSync(`${__dirname}/data/data.json`));
+const data1 = JSON.parse(
+    fs.readFileSync(`${__dirname}/data/data.json`)
+    );
 
 app.get("/api/data", (req,res)=>{
+    //send a json body back with the following data
     res.status(200).json({
-        status:"success", //optional gives message in isomnia to say it has been successfully sent
-        result: data1.length, //optional gives data on how many objects in data1
-        data: {
-            data: data1
+        status: "success", //optional - gives message in insomnia to say it has been successfully sent
+        result: data1.length, //optional - gives data on how many objects in data1
+        
+        data: {/*like an envelope for our data */
+            data/*must be named the same as endpoint*/: data1, /* everything inside of data.json (our json body of contents) */
         }
-    });
+    })
+    //.catch(err => res.status(500).json(err))
 });
 
 
 app.post("/api/data", (req,res)=>{
-    console.log(req.body); // will console the body
+    console.log(req.body); //this will console the body we input into insomnia
     
+    /*a database automatically generates id, but we are not using one 
+    so we have to manually create one in this scenario*/
     const newId = data1[data1.length - 1].id +1; //adds 1 to the id
 
+    //newData will be the body with a new id
+    //Object.assign(firstObj, secondObj) - this method merges two objects (id object with body object)
     const newData = Object.assign({id:newId}, req.body);
 
+    // will go into out data.obj file and append newData
     data1.push(newData);
-    fs.writeFile(`${__dirname}/data/data.json`,JSON.stringify(data1), err =>{
+
+    //because we are in a call back so we have to use async version (writeFile)
+    fs.writeFile(`${__dirname}/data/data.json`, JSON.stringify(data1), err =>{
         res.status(201).json({
             status: "success",
             data:{
-                data1: newData
+                data: newData
             }
         });
-    });
+    })
 });
 
 //if you do a put request you will need to send all parameters inside of that body,
 
 //with patch only the specific property that you want to updated so it saves time
 app.patch("/api/data/:id", (req,res)=>{
-    // turning into a number
+    // turning into a number because /:id is a string
     const ID = req.params.id * 1;
     //seeing if our number matches in data1 properties
     const toBeUpdated = data1.find(el => el.id === ID);
     // finding out the index of data1
     const index = data1.indexOf(toBeUpdated);
 
+    // combining the 2 objects together (note the first object (first argument) can be re-used as the merged (modified) object)
     Object.assign(toBeUpdated, req.body)
 
     data1[index] = toBeUpdated;
@@ -65,16 +78,13 @@ app.patch("/api/data/:id", (req,res)=>{
                 data1: toBeUpdated
             }
         });
-    });
+    })
 });
-
 
 app.delete("/api/data/:id", (req,res)=>{
 
     const ID = req.params.id * 1;
-
     const toBeDeleted = data1.find(el => el.id === ID);
-
     const index = data1.indexOf(toBeDeleted);
 
     data1.splice(index,1);
@@ -86,7 +96,8 @@ app.delete("/api/data/:id", (req,res)=>{
                 data1: null
             }
         });
-    });
+    })
+    .catch(err => res.status(500).json(err))
 });
 
 app.listen(PORT, ()=>{
