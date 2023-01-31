7217e76b4fb6ee0f9f40caf0586a84e5f896307d
Minhazul Islam <minhazulislam@hotmail.com>
2023-01-31 21:57:01 +0000

    edit button now renders an input over the existing text

--- public/app.js
+++ public/app.js
@@ -3,133 +3,136 @@ let inputText = document.querySelector("#inputText");
 let addButton = document.querySelector("#addButton");
 
 const createTodoItem = (value) => {
+  let output = document.createElement("p"); //creating a paragraph tag
+  output.classList.add("outputText"); //giving paragraph a class
+  output.dataset.itemId = value.id;
+  //use text content instead because inner htm is for elements
+  output.textContent = value.message; //what we want inside the paragraph
+  outputCon.appendChild(output); //where we want to add it
 
-    let output = document.createElement("p"); //creating a paragraph tag
-    output.classList.add("outputText"); //giving paragraph a class
-    //use text content instead because inner htm is for elements
-    output.textContent = value.message; //what we want inside the paragraph
-    outputCon.appendChild(output); //where we want to add it
+  let deleteBtn = document.createElement("button");
+  deleteBtn.classList.add("deleteBtn");
+  deleteBtn.innerHTML = "Delete";
+  deleteBtn.dataset.itemId = value.id; // assigning an id to delete button
+  outputCon.appendChild(deleteBtn);
 
-    let deleteBtn  =  document.createElement("button");
-    deleteBtn.classList.add("deleteBtn");
-    deleteBtn.innerHTML = "Delete";
-    deleteBtn.dataset.itemId = value.id // assigning an id to delete button
-    outputCon.appendChild(deleteBtn);
-
-    deleteBtn.addEventListener("click", (e)=>{
-
-        fetch("/api/data/" + e.target.dataset.itemId, {
-        
-            method: "DELETE",
-            
-        })
-        .then(data =>{
-            console.log(data)
-            if(data.status === 200){
-                console.log("success");
-            } 
-            if (data.status === 500){
-                console.log("something went wrong")
-            }
-        })
-        .catch(err => console.log("something went wrong"))
+  deleteBtn.addEventListener("click", (e) => {
+    fetch("/api/data/" + e.target.dataset.itemId, {
+      method: "DELETE",
     })
+      .then((data) => {
+        console.log(data);
+        if (data.status === 200) {
+          console.log("success");
+        }
+        if (data.status === 500) {
+          console.log("something went wrong");
+        }
+      })
+      .catch((err) => console.log("something went wrong"));
+  });
 
+  let editBtn = document.createElement("button");
+  editBtn.dataset.itemId = value.id; //giving edit btn an id
+  editBtn.classList.add("editBtn");
+  editBtn.innerHTML = "Edit";
+  outputCon.appendChild(editBtn);
 
-    let editBtn  =  document.createElement("button");
-    editBtn.dataset.itemId = value.id; //giving edit btn an id
-    editBtn.classList.add("editBtn");
-    editBtn.innerHTML = "Edit";
-    outputCon.appendChild(editBtn);
-   
+  editBtn.addEventListener("click", (x) => {
+    x.preventDefault();
 
-    editBtn.addEventListener("click", (x) => {
-        x.preventDefault()
-
-        let input = document.createElement("input");
-        input.type = "text";
-        input.placeholder = "enter hear to update";
-        input.className = "input";
-        outputCon.appendChild(input);
-
-        let done = document.createElement("button");
-        done.innerHTML = "done";
-        outputCon.appendChild(done);
-
-        done.addEventListener("click", (e)=>{
-            e.preventDefault();
-
-            // input.style.visibility = visible;
-            // done.style.visibility = hidden;
-
-            let updateValue = {message: input.value}
-            
-            fetch("/api/data/" + x.target.dataset.itemId, {
-                method: "PATCH",
-                headers: {
-                    "Content-Type":"application/json"
-                },
-                body: JSON.stringify(updateValue)
-            })
-            .then(response => response.json())
-            .then(data =>  {
-                console.log(data)
-
-
-                if(data.status === 200){
-                    console.log("success");
-                } 
-                if (data.status === 500){
-                    console.log("something went wrong")
-                }
-            })
-            .catch(err => console.log("something went wrong"))
-        })
-    })
-}
-
-addButton.addEventListener("click", () =>{
-    ///converts inputText.value value into an object
-    const convertToObj = {
-        message: inputText.value
-    }
+    // remove the p tag
+    const id = x.target.dataset.itemId;
+    const textEl = document.querySelector(`[data-item-id="${id}"]`);
+    const text = textEl.innerText;
+    textEl.style.display = "none";
     
-    fetch("/api/data",{
-        method:"POST", 
-        ///use this or body wont append to database (this is letting api know what type of content is being parsed)
+
+    let input = document.createElement("input");
+    input.type = "text";
+    input.placeholder = "enter hear to update";
+    input.className = "input";
+    input.style.display = "block";
+
+    // add the value of p tag to inout field
+    input.value = text;
+    outputCon.prepend(input);
+
+    let done = document.createElement("button");
+    done.innerHTML = "done";
+    outputCon.appendChild(done);
+
+    done.addEventListener("click", (e) => {
+      e.preventDefault();
+
+      // input.style.visibility = visible;
+      // done.style.visibility = hidden;
+
+      let updateValue = { message: input.value };
+
+      fetch("/api/data/" + x.target.dataset.itemId, {
+        method: "PATCH",
         headers: {
-            "Content-Type":"application/json"
+          "Content-Type": "application/json",
         },
-        // a JSON object can only take string so this method turns everything into a string
-        body: JSON.stringify(convertToObj)
-    })
-    .then(response => response.json())
-    .then(data => {
-      console.log(data)
-        createTodoItem(data)
-    })    
+        body: JSON.stringify(updateValue),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+
+          if (data.status === 200) {
+            console.log("success");
+          }
+          if (data.status === 500) {
+            console.log("something went wrong");
+          }
+        })
+        .catch((err) => console.log("something went wrong"));
+    });
+  });
+};
+
+addButton.addEventListener("click", () => {
+  ///converts inputText.value value into an object
+  const convertToObj = {
+    message: inputText.value,
+  };
+
+  fetch("/api/data", {
+    method: "POST",
+    ///use this or body wont append to database (this is letting api know what type of content is being parsed)
+    headers: {
+      "Content-Type": "application/json",
+    },
+    // a JSON object can only take string so this method turns everything into a string
+    body: JSON.stringify(convertToObj),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      createTodoItem(data);
+    });
 });
 
+const name = () => {
+  // GET method
+  fetch("/api/data", {
+    method: "GET", //is calling the get api in server.js, thats how it know which file to go inside
+  })
+    .then((response) => response.json()) //convert that data into json body
+    .then((data) => {
+      console.log(data);
+      for (let i = 0; i < data.length; i++) {
+        createTodoItem(data[i]);
+      }
 
-const name = () =>{
-    // GET method
-    fetch("/api/data", {
-        method: "GET" //is calling the get api in server.js, thats how it know which file to go inside
-    })
-    .then(response =>response.json()) //convert that data into json body
-    .then(data =>{
-        console.log(data)
-        for(let i=0; i<data.length; i++){
-            createTodoItem(data[i])
-        }
-    
-        // const items = data.data.data;
-        // // loop through all the data in 
-        // for (let i = 0; i < items.length; i++) {
-        //     const dataElement = items[i];
-        // createTodoItem(data)
-        //}
-    })
+      // const items = data.data.data;
+      // // loop through all the data in
+      // for (let i = 0; i < items.length; i++) {
+      //     const dataElement = items[i];
+      // createTodoItem(data)
+      //}
+    });
 };
-name()
-
+name();
--- server.js
+++ server.js
@@ -1,8 +1,12 @@
-/*"npm install sequelize", 
+/*
+"npm init -y"
+"npm install inquirer"
+"npm install express"
+"npm install sequelize", 
 "npm install --save mysql2", 
 "npm install dotenv --save" */
 
-//const fs = require("fs"); 
+//const fs = require("fs");
 const Message = require("./models/message");
 
 const express = require("express");
@@ -13,10 +17,10 @@ const app = express();
 
 const sequelize = require("./config/connection");
 
-app.use(express.json());// middleware to allow us to use json 
+app.use(express.json()); // middleware to allow us to use json
 app.use(express.urlencoded({ extended: true })); //middle where to allow us to use special characters in url
 
-//connects public folder
+//connects public folder and displays it to 127.0.0.1:3001
 app.use(express.static("./public"));
 
 // //reads fake database (data.json) and store it into a variable
@@ -24,117 +28,112 @@ app.use(express.static("./public"));
 //     fs.readFileSync(`${__dirname}/data/data.json`)
 //     );
 
-app.get("/api/data", (req,res)=>{
+app.get("/api/data", (req, res) => {
+  Message.findAll()
+    .then((data) => res.status(200).json(data))
+    .catch((err) => res.status(500).json(err));
 
-    Message.findAll()
-    .then(data => res.status(200).json(data))
-    .catch(err => res.status(500).json(err))
+  //send a json body back with the following data
+  // res.status(200).json({
+  //     status: "success", //optional - gives message in insomnia to say it has been successfully sent
+  //     result: data1.length, //optional - gives data on how many objects in data1
 
-    //send a json body back with the following data
-    // res.status(200).json({
-    //     status: "success", //optional - gives message in insomnia to say it has been successfully sent
-    //     result: data1.length, //optional - gives data on how many objects in data1
-        
-    //     data: {/*like an envelope for our data */
-    //         data/*must be named the same as endpoint*/: data1, /* everything inside of data.json (our json body of contents) */
-    //     }
-    // })
+  //     data: {/*like an envelope for our data */
+  //         data/*must be named the same as endpoint*/: data1, /* everything inside of data.json (our json body of contents) */
+  //     }
+  // })
 });
 
+app.post("/api/data", (req, res) => {
+  Message.create({
+    message: req.body.message,
+  })
+    .then((data) => res.status(200).json(data))
+    .catch((err) => res.status(500).json(err));
 
-app.post("/api/data", (req,res)=>{
-    Message.create({
-        message: req.body.message,
-      })
-      .then(data => res.status(200).json(data))
-      .catch(err => res.status(500).json(err))
+  // console.log(req.body); //this will console the body we input into insomnia
 
+  // /*a database automatically generates id, but we are not using one
+  // so we have to manually create one in this scenario*/
+  // const newId = data1[data1.length - 1].id +1; //adds 1 to the id
 
-    // console.log(req.body); //this will console the body we input into insomnia
-    
-    // /*a database automatically generates id, but we are not using one 
-    // so we have to manually create one in this scenario*/
-    // const newId = data1[data1.length - 1].id +1; //adds 1 to the id
+  // //newData will be the body with a new id
+  // //Object.assign(firstObj, secondObj) - this method merges two objects (id object with body object)
+  // const newData = Object.assign({id:newId}, req.body);
 
-    // //newData will be the body with a new id
-    // //Object.assign(firstObj, secondObj) - this method merges two objects (id object with body object)
-    // const newData = Object.assign({id:newId}, req.body);
+  // // will go into out data.obj file and append newData
+  // data1.push(newData);
 
-    // // will go into out data.obj file and append newData
-    // data1.push(newData);
-
-    // //because we are in a call back so we have to use async version (writeFile)
-    // fs.writeFile(`${__dirname}/data/data.json`, JSON.stringify(data1), err =>{
-    //     res.status(201).json({
-    //         status: "success",
-    //         data:{
-    //             data: newData
-    //         }
-    //     });
-    // })
+  // //because we are in a call back so we have to use async version (writeFile)
+  // fs.writeFile(`${__dirname}/data/data.json`, JSON.stringify(data1), err =>{
+  //     res.status(201).json({
+  //         status: "success",
+  //         data:{
+  //             data: newData
+  //         }
+  //     });
+  // })
 });
 
 //if you do a put request you will need to send all parameters inside of that body,
 
 //with patch only the specific property that you want to updated so it saves time
-app.patch("/api/data/:id", (req,res)=>{
+app.patch("/api/data/:id", (req, res) => {
+  Message.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((data) => res.status(200).json(data))
+    .catch((err) => res.status(500).json(err));
 
-    Message.update(req.body, {
-        where: {
-          id: req.params.id,
-        },
-      })
-      .then(data =>res.status(200).json(data))
-      .catch(err => res.status(500).json(err))
-    
-    // // turning into a number because /:id is a string
-    // const ID = req.params.id * 1;
-    // //seeing if our number matches in data1 properties
-    // const toBeUpdated = data1.find(el => el.id === ID);
-    // // finding out the index of data1
-    // const index = data1.indexOf(toBeUpdated);
+  // // turning into a number because /:id is a string
+  // const ID = req.params.id * 1;
+  // //seeing if our number matches in data1 properties
+  // const toBeUpdated = data1.find(el => el.id === ID);
+  // // finding out the index of data1
+  // const index = data1.indexOf(toBeUpdated);
 
-    // // combining the 2 objects together (note the first object (first argument) can be re-used as the merged (modified) object)
-    // Object.assign(toBeUpdated, req.body)
+  // // combining the 2 objects together (note the first object (first argument) can be re-used as the merged (modified) object)
+  // Object.assign(toBeUpdated, req.body)
 
-    // data1[index] = toBeUpdated;
-    
-    // fs.writeFile(`${__dirname}/data/data.json`,JSON.stringify(data1), err => {
-    //     res.status(200).json({
-    //         status: "success",
-    //         data:{
-    //             data1: toBeUpdated
-    //         }
-    //     });
-    // })
+  // data1[index] = toBeUpdated;
+
+  // fs.writeFile(`${__dirname}/data/data.json`,JSON.stringify(data1), err => {
+  //     res.status(200).json({
+  //         status: "success",
+  //         data:{
+  //             data1: toBeUpdated
+  //         }
+  //     });
+  // })
 });
 
-app.delete("/api/data/:id", (req,res)=>{
-    Message.destroy({
-        where: {
-            id: req.params.id,
-          },
-    })
-    .then(data =>res.status(200).json(data))
-    .catch(err => res.status(500).json(err))
+app.delete("/api/data/:id", (req, res) => {
+  Message.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((data) => res.status(200).json(data))
+    .catch((err) => res.status(500).json(err));
 
-    // const ID = req.params.id * 1;
-    // const toBeDeleted = data1.find(el => el.id === ID);
-    // const index = data1.indexOf(toBeDeleted);
+  // const ID = req.params.id * 1;
+  // const toBeDeleted = data1.find(el => el.id === ID);
+  // const index = data1.indexOf(toBeDeleted);
 
-    // data1.splice(index,1);
-    
-    // fs.writeFile(`${__dirname}/data/data.json`,JSON.stringify(data1), err => {
-    //     res.status(204).json({
-    //         status: "success",
-    //         data:{
-    //             data1: null
-    //         }
-    //     });
-    // })
+  // data1.splice(index,1);
 
+  // fs.writeFile(`${__dirname}/data/data.json`,JSON.stringify(data1), err => {
+  //     res.status(204).json({
+  //         status: "success",
+  //         data:{
+  //             data1: null
+  //         }
+  //     });
+  // })
 });
 
-sequelize.sync({ force: false }).then(()=>{
-    app.listen(PORT, () => console.log(`listening to PORT ${PORT}`))
-})
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`listening to PORT ${PORT}`));
+});
